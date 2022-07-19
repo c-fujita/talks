e1bef5ffca908d9e262bb44497f605e32fc3fe32
c-fujita <oor327@gmail.com>
2022-07-19 21:34:33 +0900

    edit table

--- database/migrations/2022_07_19_200819_references.php
+++ database/migrations/2022_07_19_200819_references.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Replies extends Migration
+class References extends Migration
 {
     /**
      * Run the migrations.
@@ -14,14 +14,14 @@ class Replies extends Migration
     public function up()
     {
         //
-        Schema::create('replies', function (Blueprint $table) {
+        Schema::create('references', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('reference_id')->unsigned();
-            $table->foreign('reference_id')->references('id')->on('references');
-            $table->string('user_name')->unsigned();
-            $table->foreign('user_name')->references('name')->on('customers');
+            $table->bigInteger('customer_id')->nullable()->unsigned();
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->string('user_name');
             $table->string('title');
             $table->string('body');
+            $table->string('tag');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
@@ -35,6 +35,6 @@ class Replies extends Migration
     public function down()
     {
         //
-        Schema::drop('replies');
+        Schema::drop('references');
     }
 }
--- database/migrations/2022_07_19_200919_replies.php
+++ database/migrations/2022_07_19_200919_replies.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class References extends Migration
+class Replies extends Migration
 {
     /**
      * Run the migrations.
@@ -14,13 +14,15 @@ class References extends Migration
     public function up()
     {
         //
-        Schema::create('references', function (Blueprint $table) {
+        Schema::create('replies', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('user_name')->unsigned();
-            $table->foreign('user_name')->references('name')->on('customers');
+            $table->bigInteger('customer_id')->nullable()->unsigned();
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->integer('reference_id')->nullable()->unsigned();
+            $table->foreign('reference_id')->references('id')->on('references');
+            $table->string('user_name');
             $table->string('title');
             $table->string('body');
-            $table->string('tag');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
@@ -34,6 +36,6 @@ class References extends Migration
     public function down()
     {
         //
-        Schema::drop('references');
+        Schema::drop('replies');
     }
 }
